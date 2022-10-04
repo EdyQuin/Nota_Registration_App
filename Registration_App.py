09e5a86c01c0de93e17b48da18d2df885901096d
Eduardo Quin <94932412+EdyQuin@users.noreply.github.com>
2022-10-03 20:33:55 -0400

    Update Registration_App.py

--- Registration_App.py
+++ Registration_App.py
@@ -2,16 +2,20 @@ import streamlit as st
 from deta import Deta
 
 st.image('./LOGO_091622.png')
+st.header('Eduardo Quin, Florida Notary Public')
+st.subheader('Please answer the following questions and submit your request below')
 
 # Data to be written to Deta Base
 with st.form("form"):
     name = st.text_input("Your name")
+    age = st.number_input("Your age")
     email = st.text_input("Your email")
-    telephone = st.text_input("Your phone number")
-    one = st.text_input("What do you need notarized, what documents, a deed, etc.?")
-    two = st.text_input("Are you working with a lawyer, please provide your lawyer's email?")
-    three = st.text_input("What is the timeline to notarize, do you need to notarize immediately?")
-    submitted = st.form_submit_button("Thank you for submitting your request. We will send you an email with further instructions shortly.")
+    telephone = st.number_input("Your phone number")
+    one = st.text_input("What document(s) do you need notarized, deed, etc?")
+    two = st.text_input("What is the timeframe you need the document(s) notarized, 1 day, 1 week?")
+    three = st.text_input("Are you working with a lawyer, please provide lawyer's email?")
+    
+    submitted = st.form_submit_button("Submit your notarization request")
     clear_on_submit=True
     
 # Connect to Deta Base with your Project Key
@@ -22,11 +26,7 @@ db = deta.Base("Notary_Registration_App")
 # write the data from the form to the database.
 # You can store any data you want here. Just modify that dictionary below (the entries between the {}).
 if submitted:
-    db.put({"name": name, "email": email, "telephone": telephone, 
+    db.put({"name": name, "age": age, "email": email, "telephone": telephone, 
             "qe": one, "qf": two, "qg": three})
     if submitted:
-        st.write("Your request for notarization has been successfully received. For any questions or concerns please contact the office @ equin@assetmana.com. Please close your browser when you are finished.")
-"---"
-# This reads all items from the database and displays them to your app.
-# db_content is a list of dictionaries. You can do everything you want with it.
-db_content = db.fetch(query=None, limit=None, last=None).items
+        st.write("Your answers have been successfully received. For any questions or concerns please contact the office @ equin@assetmana.com. Please close your browser when you are finished.")
